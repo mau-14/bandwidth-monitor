fa175938e4da1730a0d62c58d522c22532feb885
mpenaEVG <mauriciopenadominguez.guadalupe@alumnado.fundacionloyola.net>
2025-01-11 11:32:50 +0100

    añadido el print

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ last_sent = psutil.net_io_counters().bytes_sent
 last_total = last_received + last_sent
 
 
+while True:
+ 
+    bytes_received = psutil.net_io_counters().bytes_recv
+    bytes_sent = psutil.net_io_counters().bytes_sent
+    bytes_total = last_received + last_sent
+
+    new_received = bytes_received - last_received
+    new_sent = bytes_sent - last_sent
+    new_total = bytes_total - last_total
+    
+    mb_new_received = new_received / 1024 / 1024
+    mb_new_sent = new_sent / 1024 / 1024
+    mb_new_total = new_total / 1024 / 1024
+    
+    print(f"{mb_new_received:.2f} MB recieved, {mb_new_sent:.2f} MB sent, {mb_new_total:.2f} MB total")
